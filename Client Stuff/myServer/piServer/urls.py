db03df2182baf1166f45e4e00e46605116ae3b94
apo <apo@urbancode.com>
2012-11-28 16:40:58 -0500

    added create and delete functionality for alarms

--- Client Stuff/myServer/piServer/urls.py
+++ Client Stuff/myServer/piServer/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('',
     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^buildings/(?P<building_id>\d+)/$', views.building),
     url(r'^buildings/(?P<building_id>\d+)/outlets/(?P<outlet_id>\d+)/$', views.outlet),
+    url(r'^buildings/(?P<building_id>\d+)/outlets/(?P<outlet_id>\d+)/alarms/(?P<alarm_id>\d+)/delete/$', views.deleteAlarm),
+    url(r'^buildings/(?P<building_id>\d+)/outlets/(?P<outlet_id>\d+)/alarms/new/$', views.new),
+    url(r'^buildings/(?P<building_id>\d+)/outlets/(?P<outlet_id>\d+)/alarms/new/create$', views.createAlarm),
 )
--- Client Stuff/myServer/piServer/views.py
+++ Client Stuff/myServer/piServer/views.py
@@ -1,25 +1,57 @@
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from piServer.models import UserProfile, Building, Outlet, Alarm
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_list_or_404, render_to_response, get_object_or_404
+from django.core.urlresolvers import reverse
 
 @login_required
 def index(request):
     print request.user
-    building_list = get_list_or_404(Building, owner=request.user)
+    building_list = Building.objects.filter(owner=request.user)
     return render_to_response('index.html', {'building_list': building_list})
 
 def building(request, building_id):
     print building_id
     building = get_object_or_404(Building, pk=building_id)
-    outlets = get_list_or_404(Outlet, buildingID=building_id)
+    outlets = Outlet.objects.filter(buildingID=building_id)
     return render_to_response('building.html', {'building': building, 'outlets': outlets})
 
 def outlet(request, building_id, outlet_id):
     print outlet_id
     building = get_object_or_404(Building, pk=building_id)
     outlet = get_object_or_404(Outlet, pk=outlet_id)
-    alarms = get_list_or_404(Alarm, outletID=outlet_id)
+    alarms = Alarm.objects.filter(outletID=outlet_id)
     return render_to_response('outlet.html', {'building': building, 'outlet': outlet, 'alarms': alarms})
+
+def deleteAlarm(request, building_id, outlet_id, alarm_id):
+    print 'Deleting alarm with id ' + alarm_id
+    building = get_object_or_404(Building, pk=building_id)
+    outlet = get_object_or_404(Outlet, pk=outlet_id)
+    alarm = get_object_or_404(Alarm, pk=alarm_id)
+    alarm.delete()
+    return HttpResponseRedirect(reverse('piServer.views.outlet', args=(building.id, outlet.id)))
+
+def new(request, building_id, outlet_id):
+    print 'Creating an alarm for outlet ' + outlet_id
+    building = get_object_or_404(Building, pk=building_id)
+    outlet = get_object_or_404(Outlet, pk=outlet_id)
+    return render_to_response('newAlarm.html', {'building': building, 'outlet': outlet},
+                               context_instance=RequestContext(request))
+    
+def createAlarm(request, building_id, outlet_id):
+    building = get_object_or_404(Building, pk=building_id)
+    outlet = get_object_or_404(Outlet, pk=outlet_id)
+    name = request.POST['name']
+    startTime = request.POST['starttime']
+    endTime = request.POST['endtime']
+    alarm = Alarm(creator = request.user,
+                  buildingID = building,
+                  outletID = outlet,
+                  alarmName = name,
+                  startTime = startTime,
+                  endTime = endTime,
+                  desiredState = True)
+    alarm.save()
+    return HttpResponseRedirect(reverse('piServer.views.outlet', args=(building.id, outlet.id)))
